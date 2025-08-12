9a7c1e6143188ce82336c5f17e58092794dba668
Rodrigo Jimenez <jimenez.rodrigo.dev@gmail.com>
2025-08-12 00:30:40 -0400

    simplified example

--- mp_examples/basic.py
+++ mp_examples/basic.py
@@ -1,19 +1,22 @@
 import tpm
-import array
 import struct
 
-a = tpm.TPM(2,[8,2],[2,4],3,1,0,1)
-for byte in a.get_weights():
-    signed_byte = struct.unpack('b', bytes([byte]))[0]  # 'b' for signed char
-    print(signed_byte)
+tpm = TPM(
+    h=2,          # Layer count
+    k=[8,2],      # Neurons per layer
+    n=[2,4],      # Inputs per neuron for each layer
+    l=3,          # Weight range parameter. In this case (-3,3)
+    m=1,          # First layer input range. In this case (-1,1) without counting zero.
+    rule=0,       # Learning rule ID (Only Hebbian available for now)
+    scenario=0    # Scenario ID (0: No-Overlap, 1: Partial-Overlap, 2: Full-Overlap)
+)
 
+input_buf = struct.pack('16b',*[1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1])
+result = tpm.calculate(input_buf)
 
-buf = struct.pack('16b',*[1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1])
-a.calculate(buf)
+external_output = 1 #Another MTPM's calculated output
+tpm.learn(input_buf, external_output) #Learn using the external output and the last calculated output in the buffer.
 
-# /////
-a.calculate(buf)
-a.learn(buf,-1)
-for byte in a.get_weights():
-    signed_byte = struct.unpack('b', bytes([byte]))[0]  # 'b' for signed char
+for byte in tpm.get_weights():
+    signed_byte = struct.unpack('b', bytes([byte]))[0]
     print(signed_byte)
